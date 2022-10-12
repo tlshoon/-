d420b2ee9c114ef9f39d6deb53b58649eb5a5f1f
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-12 15:26:56 +0900

    Add files via upload

--- 백준.py
+++ 백준.py
@@ -249,3 +249,40 @@
 #     cnt += data[i]
 #     s += cnt
 # print(s)
+
+# 샤틀버스
+# x,y= input().split()
+# x = int(x)
+# y = int(y)
+#
+# if x>y:
+#     print(x+y)
+# else:
+#     a = y//x
+#     b = a*x
+#     print(y-b)
+
+# 2606번
+# from collections import deque
+# n = int(input())  # 컴퓨터 개수
+# v = int(input())  # 연결선 개수
+#
+# graph = [[] for _ in range(n+1)]  # 그래프 초기화
+# visited = [0] * (n+1)  # 방문한 컴퓨터인지 표시
+#
+# for i in range(v):
+#     a,b = map(int,input().split())
+#     graph[a] += [b]  # a에 b연결
+#     graph[b] += [a]  # b에 a연결  -> 양방향  // 이중리스트
+#
+# visited[1] = 1  # 1번부터 컴퓨터 시작이니 방문 표
+# q=deque([1])
+#
+# while q:
+#     start = q.popleft()
+#     for nx in graph[start]:
+#         if visited[nx] == 0:시
+#             visited[nx] = 1
+#
+# print(sum(visited)-1)
+#
