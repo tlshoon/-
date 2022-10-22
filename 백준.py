5845a717b04421b1d153b8a719ce3ddd079a1e34
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-22 15:50:21 +0900

    Add files via upload

--- 백준.py
+++ 백준.py
@@ -485,3 +485,36 @@
 # print(a)
 # for i in a:
 #     print(i,end=" ")
+
+# 1389번
+# from collections import deque
+#
+# def bfs(graph,start):
+#     num = [0] * (n+1)
+#     visited = [start]
+#     queue = deque()
+#     queue.append(start)
+#
+#     while queue:
+#         a = queue.popleft()
+#         for i in graph[a]:
+#             if i not in visited:
+#                 num[i] = num[a] + 1
+#                 visited.append(i)
+#                 queue.append(i)
+#     return sum(num)
+#
+# n,m = map(int,input().split())
+#
+# graph = [[] for _ in range(n+1)]
+#
+# for i in range(m):
+#     a,b = map(int,input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+#
+# result = []
+# for i in range(1,n+1):
+#     result.append(bfs(graph, i))
+#
+# print(result.index(min(result))+1)
