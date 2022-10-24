b63bc8a4a1a61b33e3ee0a8385c313f06ba7ee28
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-24 16:59:04 +0900

    Add files via upload

--- 백준.py
+++ 백준.py
@@ -518,3 +518,23 @@
 #     result.append(bfs(graph, i))
 #
 # print(result.index(min(result))+1)
+
+# 1302번
+# book = dict()
+# n = int(input())
+# for _ in range(n):
+#     name = input()
+#     if name in book:
+#         book[name] += 1
+#     else:
+#         book[name] = 1
+#
+# max = 0
+# sbook = dict(sorted(book.items()))
+#
+# for i in sbook:
+#     if (sbook[i]) > max:
+#         max = sbook[i]
+#         maxi = i
+# print(maxi)
+
