4cbb9d8c0ae46435bcab6e5576eacdd6a24d27dd
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-11-08 14:11:25 +0900

    Add files via upload

--- 백준.py
+++ 백준.py
@@ -1073,3 +1073,50 @@
 #     a,b = map(int,input().split())
 #     c,d = map(int,input().split())
 #     print(bfs(a,b))
+
+
+# 1743번
+# from collections import deque
+# n,m,k = map(int,input().split())
+#
+# queue = deque()
+#
+# dx = [-1,1,0,0]
+# dy = [0,0,-1,1]
+#
+# visited = [[0] * m for _ in range(n)]
+#
+# graph = [[0] * m for _ in range(n)]
+#
+# for i in range(k):
+#     a,b = map(int,input().split())
+#     graph[a-1][b-1] = 1
+#
+#
+# def bfs(x,y):
+#     cnt = 0
+#     visited[x][y] = 1
+#     queue.append([x,y])
+#     while queue:
+#         x,y = queue.popleft()
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or nx >= n or ny < 0 or ny >= m:
+#                 continue
+#             if visited[nx][ny] == 1:
+#                 continue
+#             if graph[nx][ny] == 1:
+#                 visited[nx][ny] = 1
+#                 queue.append([nx,ny])
+#                 cnt += 1
+#     return cnt + 1
+#
+# max_food = 0
+# for i in range(n):
+#     for j in range(m):
+#         if graph[i][j] == 1 and visited[i][j] == 0:
+#             max_food = max(max_food, bfs(i,j))
+# print(max_food)
+
+
