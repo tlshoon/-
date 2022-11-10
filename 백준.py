34576b7066f20ae752f10a0721e934b4d552c48a
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-11-11 02:14:35 +0900

    Add files via upload

--- 백준.py
+++ 백준.py
@@ -1155,3 +1155,15 @@
 #         cnt += score[i] - score[i+1] + 1
 #         score[i] = score[i+1]-1
 # print(cnt)
+
+# 11053번
+# n = int(input())
+# array = list(map(int,input().split()))
+# dp = [0 for _ in range(n)]
+#
+# for i in range(n):
+#     for j in range(i):
+#         if array[i] > array[j] and dp[i] < dp[j]:
+#             dp[i] = dp[j]
+#     dp[i] += 1
+# print(max(dp))
