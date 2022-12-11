64a9ee828a05b48b15f728c0c9080a8c492cf9b4
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-12-11 23:52:54 +0900

    Add files via upload

--- 백준.py
+++ 백준.py
@@ -1193,3 +1193,28 @@
 #     arr[i] = max(arr[i], arr[i]*arr[i-1])
 #
 # print('%0.3f'%max(arr))
+
+# 1793번
+# n = int(input())
+#
+# d = [0] * 251
+#
+# d[1] = 1
+# d[2] = 3
+#
+# for i in range(3,n+1):
+#     d[i] = d[i-1] + (2 * d[i-2])
+#
+# print(d[n])
+
+# 11726번
+# n = int(input())
+#
+# d = [0] * 1001
+# d[1] = 1
+# d[2] = 2
+#
+# for i in range(3,n+1):
+#     d[i] = (d[i-1] + d[i-2]) % 10007
+#
+# print(d[n])
